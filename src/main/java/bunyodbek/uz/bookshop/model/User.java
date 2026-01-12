a843077b1f2601ae1e228cf7575874ecdf938c2b
Bunyodbek Nurmamatov <bunyodbeknurmamatov@gmail.com>
2026-01-12 17:19:53 +0500

    add Project

---
+++ src/main/java/bunyodbek/uz/bookshop/model/User.java
@@ -0,0 +1,21 @@
+package bunyodbek.uz.bookshop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private Long chatId;
+
+    private String phoneNumber;
+}
